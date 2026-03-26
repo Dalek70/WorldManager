6a2aa1b0bc8c51f4eadb7d08ffe1b48a70663064
Dalek70 <Isaacnumber5@outlook.com>
2026-03-26 08:50:04 +0800

    Saving worlds
    Just added in a few methods and hooks/listeners to save the worlds on server crash/stop and then load the worlds back up when the server starts.

--- src/main/java/me/dalek70/worldmanager/WorldManager.java
+++ src/main/java/me/dalek70/worldmanager/WorldManager.java
@@ -3,6 +3,9 @@ package me.dalek70.worldmanager;
 import me.dalek70.worldmanager.commands.CleanWorldManager;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.mineacademy.fo.Common;
 import org.mineacademy.fo.model.Tuple;
 import org.mineacademy.fo.plugin.SimplePlugin;
@@ -23,10 +26,36 @@ public class WorldManager extends SimplePlugin {
 		WorldManager.worldDeletionTimeout.replace(world, new Tuple<Integer, Long> (WorldManager.worldDeletionTimeout.get(world).getKey(), System.currentTimeMillis()));
 	}
 
+	private void saveCreatedWorlds() {
+		getConfig().set("created-worlds", createdWorlds);
+		saveConfig();
+		Bukkit.savePlayers();
+		for(String worldName : createdWorlds) {
+			Bukkit.unloadWorld(worldName, true);
+		}
+		Common.log("Saved " + createdWorlds.size() + " world(s) to config");
+	}
+
+	public void loadCreatedWorlds() {
+		List<String> worlds = getConfig().getStringList("created-worlds");
+		createdWorlds.clear();
+		for(String worldName : worlds) {
+			WorldCreator worldCreator = new WorldCreator(worldName);
+			worldCreator.generator(new VoidChunkGenerator());
+		}
+		createdWorlds.addAll(worlds);
+		Common.log("Loaded " + worlds.size() + " world(s) from config");
+	}
+
 	@Override
 	protected void onPluginStart() {
 		// start up the settings class for this plugin
 		Settings.init(this);
+
+		// add shutdown hook for if the server crashes
+		Runtime.getRuntime().addShutdownHook(new Thread(this::saveCreatedWorlds));
+
+		loadCreatedWorlds();
 
 		// try to start the code
 		try {
@@ -37,6 +66,7 @@ public class WorldManager extends SimplePlugin {
 					long lastVisitTime = worldDeletionTimeout.get(world).getValue();
 					if (Util.secondsSinceLastVisit(lastVisitTime) >= timeout) {
 						Common.log("Deleting world " + world.getName() + " due to inactivity");
+						Util.removePlayersFromWorld(world);
 						Util.deleteWorldFolder(world.getWorldFolder());
 						worldDeletionTimeout.remove(world);
 						createdWorlds.remove(world.getName());
@@ -49,5 +79,10 @@ public class WorldManager extends SimplePlugin {
 			Common.warning("Disabling plugin");
 			Bukkit.getPluginManager().disablePlugin(this);
 		}
+	}
+
+	@Override
+	protected void onPluginStop() {
+		saveCreatedWorlds();
 	}
 }
